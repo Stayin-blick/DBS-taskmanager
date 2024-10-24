b15390e36e37da76b51759cc4a8ec70d97980212
Azeez Bello <azeezbello75@gmail.com>
2024-10-24 06:01:45 +0000

    CR functionality complete

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -10,12 +10,13 @@ def home():
 
 @app.route("/categories")
 def categories():
-    return render_template("categories.html")
+    categories = list(Category.query.order_by(Category.category_name).all())
+    return render_template("categories.html", categories = categories)
 
 
-@app.route("/add_categories", methods=["GET", "POST"])
-def add_categories():
-    if request.methods == "POST":
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
         category = Category(category_name=request.form.get("category_name"))
         db.session.add(category)
         db.session.commit()
