da55d59586f49a1bafa0519d59b89e6acd148759
Azeez Bello <azeezbello75@gmail.com>
2024-10-27 14:21:33 +0000

    task create and read functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,27 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let sidenav = document.querySelectorAll('.sidenav');
-    M.Sidenav.init(sidenav);
+document.addEventListener('DOMContentLoaded', function () {
+  // side nav intialization
+  let sidenav = document.querySelectorAll('.sidenav');
+  M.Sidenav.init(sidenav);
+
+  // modal intialization
+  let modal = document.querySelectorAll('.modal');
+  M.Modal.init(modal);
+
+  // datepicker intialization
+  let datepicker = document.querySelectorAll('.datepicker');
+  M.Datepicker.init(datepicker, {
+    autoClose: true,
+    // format: "yyyy-mm-dd",
+    i18n: {
+      done: "Select"
+    }
   });
+
+  // form select intialization
+  let selects = document.querySelectorAll('select');
+  M.FormSelect.init(selects);
+
+  // collapsible initialization
+  let collapsible = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(collapsible);
+});
